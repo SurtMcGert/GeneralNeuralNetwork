76f60e8a7486fcd42a3d40e12c8831e35615e472
SurtMcGert <harry.ayre@outlook.com>
2021-07-20 20:41:06 +0100

    first commit

---
+++ src/generalneuralnetwork/GeneralNeuralNetwork.java
@@ -0,0 +1,34 @@
+package generalneuralnetwork;
+
+import NeuralNetwork.Matrix;
+import NeuralNetwork.NeuralNetwork;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author harry
+ */
+public class GeneralNeuralNetwork {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.lang.Exception
+     */
+
+    public GeneralNeuralNetwork() {
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        new GeneralNeuralNetwork();
+    }
+
+}
